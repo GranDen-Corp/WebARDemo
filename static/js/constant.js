1a93fde93309450eee4dffb5a6fce6fb3b6523a1
施華桐 <gundambox@granden.rocks>
2019-11-06 09:44:31 +0800

    split index.html to allton.html, granden.html, oready.html

--- static/js/constant.js
+++ static/js/constant.js
@@ -4,23 +4,17 @@ export const MODEL_FOLDER = `static/model/`;
 
 export const fbxFile = `Cha_AJOJO_001_test_191025.fbx`;
 export const pngFile = `Cha_AJOJO_1001.png`;
-export const tgaFile = `Cha_AJOJO_1001.tga`;
-export const exampleFile = `SambaDancing.fbx`;
 
 export const CAMERA_PARAM = `static/data/camera_para.dat`;
 
 export const MARKER_FOLDER = `static/marker/pattern`;
-export const HIRO_MARKER_PATTERN = `${MARKER_FOLDER}/patt.hiro`;
-export const KANJI_MARKER_PATTERN = `${MARKER_FOLDER}/patt.kanji`;
-export const RED_MARKER_PATTERN = `${MARKER_FOLDER}/red.patt`;
-export const GREEN_MARKER_PATTERN = `${MARKER_FOLDER}/green.patt`;
-export const BLUE_MARKER_PATTERN = `${MARKER_FOLDER}/blue.patt`;
-export const PYTHON_MARKER_PATTERN = `${MARKER_FOLDER}/pattern-Python_logo-512.patt`;
-export const VSCODE_MARKER_PATTERN = `${MARKER_FOLDER}/pattern-vs_code_icon.patt`;
-export const GITHUB_MARKER_PATTERN = `${MARKER_FOLDER}/pattern-github-512.patt`;
-export const GRANDEN_MARKER_PATTERN = `${MARKER_FOLDER}/pattern-granden_logo.patt`;
+// export const HIRO_MARKER_PATTERN = `${MARKER_FOLDER}/patt.hiro`;
+// export const KANJI_MARKER_PATTERN = `${MARKER_FOLDER}/patt.kanji`;
+export const GRANDEN_MARKER_PATTERN = `${MARKER_FOLDER}/pattern-Python_logo-512.patt`;
+export const ALLTON_MARKER_PATTERN = `${MARKER_FOLDER}/pattern-github-512.patt`;
+export const OREADY_MARKER_PATTERN = `${MARKER_FOLDER}/pattern-vs_code_icon.patt`;
 
 export const IMG_FOLDER = `static/img/`;
-export const DESCRIPTION_FILE1 = `description1.png`;
-export const DESCRIPTION_FILE2 = `description2.png`;
-export const DESCRIPTION_FILE3 = `description3.png`;
+export const GRANDEN_DESCRIPTION_FILE = `granden_description.png`;
+export const ALLTON_DESCRIPTION_FILE = `allton_description.png`;
+export const OREADY_DESCRIPTION_FILE = `oready_description.png`;
